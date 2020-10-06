7476d17609a78150e44a90b92fac231f2072aec5
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2020-10-07 01:53:24 +0300

    fixed CI & tests

--- scripts/install_gtest.sh
+++ scripts/install_gtest.sh
@@ -1,13 +1,21 @@
+#!/bin/bash
+
+UBUNTU_VERSION=$(lsb_release -r -s)
+
 sudo apt-get install libgtest-dev
-cd /usr/src/googletest/googletest || exit
-sudo mkdir build
-cd build || exit
+
+if [ "$UBUNTU_VERSION" = "16.04" ]; then
+  cd /usr/src/googletest/googletest || exit
+else
+  cd /usr/src/gtest || exit
+fi
+
+sudo mkdir gtest-build
+cd gtest-build || exit
 sudo cmake ..
 sudo make
 sudo cp libgtest* /usr/lib/
 cd ..
-sudo rm -rf build
-
 sudo mkdir /usr/local/lib/googletest
 sudo ln -s /usr/lib/libgtest.a /usr/local/lib/googletest/libgtest.a
 sudo ln -s /usr/lib/libgtest_main.a /usr/local/lib/googletest/libgtest_main.a
