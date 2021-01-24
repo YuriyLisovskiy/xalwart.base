f1349a7c9ab32e9cfc4b90b80bd865611d574632
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2021-01-24 13:14:57 +0200

    tested text::smart_split

---
+++ tests/tests_text.cpp
@@ -0,0 +1,54 @@
+/**
+ * tests_text.cpp
+ *
+ * Copyright (c) 2021 Yuriy Lisovskiy
+ */
+
+#include <gtest/gtest.h>
+
+#include "../src/text.h"
+
+using namespace xw;
+
+
+TEST(TestCase_smart_split, Test_DoubleQuotes)
+{
+	std::string input = R"(This is "a person's" test.)";
+	std::vector expected = {
+		"This", "is", R"("a person's")", "test."
+	};
+	auto actual = text::smart_split(input);
+	ASSERT_EQ(expected.size(), actual.size());
+	for (size_t i = 0; i < expected.size(); i++)
+	{
+		ASSERT_EQ(expected[i], actual[i]);
+	}
+}
+
+TEST(TestCase_smart_split, Test_SingleQuotes)
+{
+	std::string input = R"(Another 'person\'s' test.)";
+	std::vector expected = {
+		"Another", R"('person\'s')", "test."
+	};
+	auto actual = text::smart_split(input);
+	ASSERT_EQ(expected.size(), actual.size());
+	for (size_t i = 0; i < expected.size(); i++)
+	{
+		ASSERT_EQ(expected[i], actual[i]);
+	}
+}
+
+TEST(TestCase_smart_split, Test_NestedDoubleQuotes)
+{
+	std::string input = R"(A "\"funky\" style" test.)";
+	std::vector expected = {
+		"A", R"("\"funky\" style")", "test."
+	};
+	auto actual = text::smart_split(input);
+	ASSERT_EQ(expected.size(), actual.size());
+	for (size_t i = 0; i < expected.size(); i++)
+	{
+		ASSERT_EQ(expected[i], actual[i]);
+	}
+}
