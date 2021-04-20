318d7822940b3b0f476db32bac9674a7940339c5
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2021-04-21 02:35:52 +0300

    fix

--- src/types/_def_.h
+++ src/types/_def_.h
@@ -15,5 +15,5 @@
 #define __TYPES_END__ } __MAIN_NAMESPACE_END__
 
 // xw::types::abc
-#define __TYPES_ABC_BEGIN__ __MAIN_NAMESPACE_BEGIN__ namespace abc {
-#define __TYPES_ABC_END__ } __MAIN_NAMESPACE_END__
+#define __TYPES_ABC_BEGIN__ __TYPES_BEGIN__ namespace abc {
+#define __TYPES_ABC_END__ } __TYPES_END__
